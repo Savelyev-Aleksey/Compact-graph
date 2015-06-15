baa57c67680ac7fcbd6ca7b6586b912b2e5d3531
aleksey <saleksey92@gmail.com>
2015-06-15 22:27:15 +0600

    Shortest loop algorythm optimization.

--- graph/Projection.cpp
+++ graph/Projection.cpp
@@ -420,7 +420,8 @@ void Projection::updateShortestLoop()
 
             if (!find)
             {
-                for(++replN; replN != endL; ++replN)
+                for(++replN; replN != endL && shortestLoop < origN + replN;
+                    ++replN)
                 {
                     const auto &repLevel = *levelList->at(replN);
                     const auto repB = repLevel.begin();
